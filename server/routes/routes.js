2b05323c15c04dac0757aa82217c2c74c5cb211b
marcos <marcos@localhost.localdomain>
2019-03-21 22:28:02 -0300

    added sport routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+
 const sportsmanRouter = require('./sportsman');
+const sportRouter = require('./sport');
 
 router.use('/sportsman', sportsmanRouter);
+router.use('/sport', sportRouter);
 
 module.exports = router;
---
+++ server/routes/sport.js
@@ -0,0 +1,21 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.put('/', (req, res) => {
+
+});
+
+module.exports = router;
