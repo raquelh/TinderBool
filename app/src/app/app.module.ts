84b87da8d7a9f39519dce127b6d17026cdec1a6b
Andrew Malta <andrewsaxx@gmail.com>
2019-03-30 20:39:46 -0300

    Creating API-Rest Service

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ app/src/app/rest-api.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
+import jsSHA from 'jssha';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'}),
+};
+const apiUrl = "https://jsonplaceholder.typicode.com";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class RestApiService {
+
+  constructor(private http: HttpClient) { }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('Um erro ocorreu:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(`API retornou o código ${error.status}: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError('Algo de errado ocorreu, tente novamente em alguns minutos.');
+  }
+
+  private extractData(res: Response) {
+    let body = res;
+    return body || { };
+  }
+
+  encrypt(text: string) {
+    let shaObj = new jsSHA("SHA-256", "TEXT");
+    shaObj.update(text);
+    return shaObj.getHash("HEX");
+  }
+
+  login(email: string, password: string): Observable<any> {
+    const url = `${apiUrl}/users?email=${email}`;
+    let passwordHash = this.encrypt(password);
+    console.log(passwordHash);
+    return this.http.get(url, httpOptions).pipe(
+      map(this.extractData),
+      catchError(this.handleError)
+    );
+  }
+
+}
