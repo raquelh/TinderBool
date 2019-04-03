3617eba2ec56cb14c6810f4a3daba50929a30cd9
Estela <estelavilasboas01@gmail.com>
2019-04-02 21:58:08 -0300

    atualizando

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'config-members', loadChildren: './config-members/config-members.module#ConfigMembersPageModule' },
-  { path: 'config-private-team', loadChildren: './config-private-team/config-private-team.module#ConfigPrivateTeamPageModule' }
+  { path: 'config-private-team', loadChildren: './config-private-team/config-private-team.module#ConfigPrivateTeamPageModule' },
+  { path: 'initial', loadChildren: './initial/initial.module#InitialPageModule' }
 
 ];
 @NgModule({
