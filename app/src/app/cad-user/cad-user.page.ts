90a795c566a4d417baaa127748a44735790b7b9d
Estela <estelavilasboas01@gmail.com>
2019-04-04 20:13:42 -0300

    no more errors

--- app/src/app/cad-user/cad-user.page.ts
+++ app/src/app/cad-user/cad-user.page.ts
@@ -2,15 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { UsernameValidator } from "../../../validators/username.validator";
 import { PasswordValidator } from "../../../validators/password.validator";
-
+/*
 @Component({
   selector: 'app-cad-user',
   templateUrl: './cad-user.page.html',
   styleUrls: ['./cad-user.page.scss'],
-})
+});
+
 export class CadUserPage implements OnInit {
-  [x: string]: FormControl;
   validate-user: FormGroup;
+
   matching_passwords_group: FormGroup;
 
   constructor() { }
@@ -19,7 +20,8 @@ export class CadUserPage implements OnInit {
     this.validate-user = this.formBuilder.group({
       this.name= new FormControl('', Validators.required)
     });
-    
+
   };
 
 }
+*/
