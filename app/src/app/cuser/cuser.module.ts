a32d2f51921d3676b5de8998f54dd32461deed4e
Estela <estelavilasboas01@gmail.com>
2019-04-04 22:10:06 -0300

    login ta okay

--- app/src/app/cuser/cuser.module.ts
+++ app/src/app/cuser/cuser.module.ts
@@ -5,24 +5,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
-<<<<<<< HEAD:app/src/app/cuser/cuser.module.ts
 import { CuserPage } from './cuser.page';
 
 const routes: Routes = [
   {
     path: '',
     component: CuserPage
-=======
-import { CadUserPage } from './cad-user.page';
-/*
-const routes: Routes = [
-  {
-    path: '',
-    component: CadUserPage,
->>>>>>> e2f4530d0a96cf4e8c159767b3178b62c4ffd97b:app/src/app/cad-user/cad-user.module.ts
   }
-];
-*/
+]
+
 @NgModule({
   imports: [
     CommonModule,
