4a5aa2c82effc2467fbb9d0cbfa78d9939628ba1
lala <lala@gmail.com>
2019-04-04 22:16:54 -0300

    ainda esta dando erro

--- app/src/app/cuser/cuser.module.ts
+++ app/src/app/cuser/cuser.module.ts
@@ -1,34 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Router } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
-<<<<<<< HEAD:app/src/app/cuser/cuser.module.ts
 import { CuserPage } from './cuser.page';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: CuserPage
-=======
-import { CadUserPage } from './cad-user.page';
-/*
-const routes: Routes = [
-  {
-    path: '',
-    component: CadUserPage,
->>>>>>> e2f4530d0a96cf4e8c159767b3178b62c4ffd97b:app/src/app/cad-user/cad-user.module.ts
-  }
-];
-*/
+import { AppRoutingModule } from '../app-routing.module';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    // RouterModule.forChild()
   ],
   declarations: [CuserPage]
 })
